bc5dc76f7c9b30d3d78a4a4e3a76809efd8e0c84
imatheusmendonca <120023913+imatheusmendonca@users.noreply.github.com>
2023-02-15 12:05:05 -0300

    Add files via upload

---
+++ Aula27Janeiro/ContaCorrente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula27Janeiro
+{
+    public class ContaCorrente
+    {
+        public int agencia = 0;
+        public string conta = "";
+        public string nome = "";
+        public double saldo = 0.0;
+
+        public void Depositar (double valor)
+        {
+            this.saldo += valor;
+        }
+
+    }
+
+
+
+}
---
+++ Aula27Janeiro/Program.cs
@@ -0,0 +1,26 @@
+﻿using Aula27Janeiro;
+
+// criando o objeto que acessa os atributos da classe
+
+ContaCorrente conta1 = new ContaCorrente();
+ContaCorrente conta2 = new ContaCorrente();
+ContaCorrente conta3 = new ContaCorrente();
+
+// adicionando valores aos atributos do objeto 
+
+conta1.nome = "Matheus";
+conta1.agencia = 215;
+conta1.saldo = 8500.00;
+conta1.conta = "22975";
+conta1.Depositar(500);
+
+conta2.nome = "Pedro";
+conta2.agencia = 200;
+conta2.saldo = 100.00;
+conta2.conta = "25108";
+
+conta3.nome =  "Luiza"
+conta3.agencia = 100;
+conta3.saldo = 58.90;
+conta3.conta = "2200";
+
